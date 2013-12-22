cf5d43b90c8b528e85191f1d8d650f19e61b78b4
Bezumkin <bezumkin@yandex.ru>
2013-12-22 13:37:55 +0700

    Removed unnecessary files

--- core/components/jevix/controllers/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @package jevix
- * @subpackage controllers
- */
-require_once dirname(dirname(__FILE__)).'/model/jevix/jevix.class.php';
-$jevix = new Jevix($modx);
-return $jevix->initialize('mgr');
--- core/components/jevix/model/jevix/mysql/jevixitem.class.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * @package jevix
- */
-require_once (strtr(realpath(dirname(dirname(__FILE__))), '\\', '/') . '/jevixitem.class.php');
-class JevixItem_mysql extends JevixItem {}
