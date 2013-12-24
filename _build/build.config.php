9e1d0a02b29fd0312772273ee19fc6d4c60a0328
Bezumkin <bezumkin@yandex.ru>
2013-12-24 13:21:25 +0700

    Version 1.2.0-pl2

--- _build/build.config.php
+++ _build/build.config.php
@@ -5,7 +5,7 @@ define('PKG_NAME','Jevix');
 define('PKG_NAME_LOWER',strtolower(PKG_NAME));
 
 define('PKG_VERSION','1.2.0');
-define('PKG_RELEASE','pl1');
+define('PKG_RELEASE','pl2');
 define('PKG_AUTO_INSTALL', true);
 define('PKG_NAMESPACE_PATH', '{core_path}components/'.PKG_NAME_LOWER.'/');
 
